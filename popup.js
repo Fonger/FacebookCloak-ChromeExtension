20584ec58c9235f43470d565dbb1cb23a7b363fe
Fonger <fonger@users.noreply.github.com>
2017-10-22 22:54:07 +0800

    fix checkbox default value

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
     chrome.runtime.sendMessage({ enabled: onOffSwitch.checked ? true : false });
   });
 
-  chrome.storage.sync.get('enabled', function(data) {
+  chrome.storage.sync.get({ 'enabled': true }, function(data) {
     if (data.enabled) onOffSwitch.checked = 'checked';
   })
 });
